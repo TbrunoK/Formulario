8c3b3d1747bbef38e0266628f4aebcbf8626742a
Bruno Chagas <tbrunok@hotmail.com>
2024-05-26 17:32:31 -0300

    Correção do grid

--- js/event-details.js
+++ js/event-details.js
@@ -22,17 +22,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Cria o HTML com os detalhes do evento
   const eventDetailsHTML = `
-        <h2 class="event-title">${title}</h2>
-        <p><strong>Link do evento:</strong> <a href="${link}" target="_blank">${link}</a></p>
-        <p><strong>Whatsapp para contato:</strong> ${whatsapp}</p>
-        <p><strong>Descrição:</strong> ${info}</p>
-        <p><strong>Categoria:</strong> ${category}</p>
-        <p><strong>E-mail do administrador:</strong> ${email}</p>
-        <p><strong>Senha de acesso:</strong> ${password}</p>
-        <p><strong>Data:</strong> ${date}</p>
-        <p><strong>Início:</strong> ${begin}</p>
-        <p><strong>Fim:</strong> ${end}</p>
-    `
+    <h2 class="event-title">${title}</h2>
+    <div class="details-grid">
+      <p><strong>Link do evento:</strong></p> <p><a class="infos" href="${link}" target="_blank">${link}</a></p>
+      <p><strong>Whatsapp para contato:</strong></p> <p class="infos">${whatsapp}</p>
+      <p><strong>Descrição:</strong></p> <p class="infos">${info}</p>
+      <p><strong>Categoria:</strong></p> <p class="infos">${category}</p>
+      <p><strong>E-mail do administrador:</strong></p> <p class="infos">${email}</p>
+      <p><strong>Senha de acesso:</strong></p> <p class="infos">${password}</p>
+      <p><strong>Data:</strong></p> <p class="infos">${date}</p>
+      <p><strong>Início:</strong></p> <p class="infos">${begin}</p>
+      <p><strong>Fim:</strong></p> <p class="infos">${end}</p>
+    </div>
+  `
 
   // Insere o HTML no container
   eventDetailsContainer.innerHTML = eventDetailsHTML
